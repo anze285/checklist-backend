885a23d0d311474840fb91dc478bcf697a7c3a63
anze285 <anze285@users.noreply.github.com>
2021-01-13 23:28:18 +0100

    database connected

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors')
+const cors = require('cors');
+
+const config = require('./config/config');
 
 mongoose.connect(config.db, {
     useNewUrlParser: true,
