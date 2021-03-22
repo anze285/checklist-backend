fde2444c18c60d0d7fe907f9613e8a28b13146cd
anze285 <anze.gorsek@gmail.com>
2021-03-22 14:12:39 +0100

    working on google api

--- index.js
+++ index.js
@@ -31,14 +31,17 @@ const user = require('./routes/user')
 const project = require('./routes/project')
 const item = require('./routes/item')
 const status = require('./routes/status')
-const google = require('./routes/google')
+//const google = require('./routes/google')
 
 app.use("/api/user", user)
 app.use("/api/project", project)
 app.use("/api/item", item)
 app.use("/api/status", status)
-app.use("/api/google", google)
+//app.use("/api/google", google)
 
+//OAUTH2CALLBACK
+
+app.use("/oauth2callback", require('./oauth2callback'))
 
 //LISTENING//
 
