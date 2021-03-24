6082df187cd30a28d0473d5109f7b1a96518d095
anze285 <anze.gorsek@gmail.com>
2021-03-24 09:47:27 +0100

    now googl drive api actually works

--- oauth2callback.js
+++ oauth2callback.js
@@ -17,15 +17,9 @@ const TOKEN_PATH = 'token.json';
 router.get("/", function (req, res){
   // Load client secrets from a local file.
   fs.readFile('credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
+    if (err) return res.send('Error loading client secret file:', err);
     // Authorize a client with credentials, then call the Google Drive API.
-    if(req.query.code != undefined && req.query.code != null){
-      authorize(JSON.parse(content), listFiles, req.query.code);
-      res.sendStatus(200);
-    }
-    else{
-      authorizeApp(JSON.parse(content), res)
-    }
+    authorize(JSON.parse(content), listFiles, req.query.code, res);
   });
 })
 
@@ -35,40 +29,27 @@ router.get("/", function (req, res){
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback, code) {
+function authorize(credentials, callback, code, res) {
   const {client_secret, client_id, redirect_uris} = credentials.web;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return storeAccessToken(oAuth2Client, callback, code);
+    if (err) return getAccessToken(oAuth2Client, callback, code, res);
     oAuth2Client.setCredentials(JSON.parse(token));
     callback(oAuth2Client);
+    //res.redirect(oAuth2Client.generateAuthUrl({ access_type: 'offline', scope: SCOPES, }));
+    res.sendStatus(200)
   });
 }
 
-function authorizeApp(credentials, res) {
-  const {client_secret, client_id, redirect_uris} = credentials.web;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
-  // Check if we have previously stored a token.
+
+function getAccessToken(oAuth2Client, callback, code, res) {
   const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES,
+    access_type: 'offline',
+    scope: SCOPES,
   });
-  res.redirect(authUrl);
-  //console.log('Authorize this app by visiting this url:', authUrl);
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-
-function storeAccessToken(oAuth2Client, callback, code){
-
+  if(code != undefined && code != null){
     oAuth2Client.getToken(code, (err, token) => {
       if (err) return console.error('Error retrieving access token', err);
       oAuth2Client.setCredentials(token);
@@ -78,8 +59,19 @@ function storeAccessToken(oAuth2Client, callback, code){
         console.log('Token stored to', TOKEN_PATH);
       });
       callback(oAuth2Client);
+      res.sendStatus(200);
     });
+  }
+  else{
+    res.redirect(authUrl);
+  }
 }
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
 
 /**
  * Lists the names and IDs of up to 10 files.
@@ -93,6 +85,7 @@ function listFiles(auth) {
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
     const files = res.data.files;
+    console.log(data);
     if (files.length) {
       console.log('Files:');
       files.map((file) => {
