62c8c1b84d34cc785a19d5533cddc6960083eb9f
anze285 <anze.gorsek@gmail.com>
2021-04-20 21:44:06 +0200

    Update oauth2callback.js

--- oauth2callback.js
+++ oauth2callback.js
@@ -21,7 +21,6 @@ const SCOPES = ['https://www.googleapis.com/auth/drive.file', 'https://www.googl
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = 'token.json';
 
 router.use(cors())
 
